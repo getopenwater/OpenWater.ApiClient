b26fd89bf40ae6e8bf87785b0bf93d22c76be9a3
Vladislav Dyshakov <VADyshakov@gmail.com>
2019-08-26 12:54:13 +0700

    Removed api keys from ApiClient.Samples (for #7).

--- OpenWater.ApiClient.Samples/Program.cs
+++ OpenWater.ApiClient.Samples/Program.cs
@@ -8,7 +8,6 @@ namespace OpenWater.ApiClient.Samples
 
         private static void Main()
         {
-            ApiClient = new OpenWaterApiClient("demo.secure-platform.com", "41c41e1c-9786-4f79-be44-8e14d4fdc95b");
             //ApiClient = new OpenWaterApiClient("localhost", "some api key", baseUrl: "http://localhost:11100");
 
             //UserSamples.GetAllUsersAsync();
